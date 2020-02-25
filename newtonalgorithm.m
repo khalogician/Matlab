c9876afcc7b60ba6e49e3f6fa0880e975584ad04
Khawla <41359810+khalogician@users.noreply.github.com>
2020-02-25 09:05:52 +0400

    Update newtonalgorithm.m

--- newtonalgorithm.m
+++ newtonalgorithm.m
@@ -1,3 +1,4 @@
+% newton algorithm is a method of approximating roots of functions
 function x = mynewton (f,fd ,x0 ,n)
 format long                                       % prints more digits
 x = x0;                                           % set x equal to the initial guess x0
@@ -5,4 +6,7 @@ for i = 1:n                                       % Do n times
 x = x - f(x)/ fd(x)                               % Newton ’s formula , prints x too
 end
 
-% to 
+% to test the function do the following:
+% f = @(x)x.^2-5
+% fp = @(x)2*x
+% mynewton(f,fp,4,5)

c9f04a51cab5e84bce42939765e15d6d99e3d816
Khawla <41359810+khalogician@users.noreply.github.com>
2020-02-28 15:20:40 +0400

    Update mysecantwhile.m

--- mysecantwhile.m
+++ mysecantwhile.m
@@ -7,7 +7,7 @@ i=0;
 disp('#-------- Iter.  Error        Solution ');
 while r > tol % if while statement true iterates again
     fd = (f(x1)-f(x0))/(x1-x0);
-   x2 = x1 - f(x1)/ fd;
+    x2 = x1 - f(x1)/ fd;
     r = abs (x2- x1);
     x0 = x1;
     x1 = x2;

fe273d5a14f2880e49c86a7b6c0672c7f1b121e8
Khawla <41359810+khalogician@users.noreply.github.com>
2020-03-03 08:46:20 +0400

    Update secant.m

--- secant.m
+++ secant.m
@@ -1,3 +1,4 @@
+% Date: 03/03/2020
 function secantmethod(f, x0,x1,n,tol);
 r = 10;
 for i = 1:n
@@ -6,8 +7,10 @@ if r > tol
     x2 = x1 - f(x1) / fd;
     r = abs(f(x2));
     disp ( sprintf ( '\t\t %3d \t %.7f \t  %.7f\t ', i,r,x2));
+    % assign the errors to a vector so that we can display it
+    % and see how close we're getting to the actual value
     E(i)=r;
-    else break;
+else break;
     end
     x0 = x1;    x1=x2;
 end
